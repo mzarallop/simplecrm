a7763e84cb21f6561ab1e464e92876e1f3561c3f
MZARALLOP <mzarallop@gmail.com>
2016-09-28 00:29:43 -0300

    creando form 2

--- application/views/marketing/index.php
+++ application/views/marketing/index.php
@@ -1,11 +1,11 @@
 <div class="row-fluid">
 	<div class="span3">
-		Segmentos:<br>
-		<select name="segmento" id="segmento"></select>
-		Regiónes:<br>
-		<select name="region" id="region"></select>
-		Comunas:<br>
-		<select name="comuna" id="comuna"></select>
+		<label>Segmentos:<br>
+		<select name="segmento" id="segmento"></select></label>
+		<label>Regiónes:<br>
+		<select name="region" id="region"></select></label>
+		<label>Comunas:<br>
+		<select name="comuna" id="comuna"></select></label>
 	</div>
 	<div class="span9">
 		
