0b5c92215e7ee44995c7ce832c64844451958cef
MZARALLOP <mzarallop@gmail.com>
2016-09-28 00:51:49 -0300

    creando form 6 CAMBIOS EN EL MODEL

--- application/models/mod_marketing.php
+++ application/models/mod_marketing.php
@@ -2,10 +2,10 @@
 class mod_marketing extends CI_Model{
 
 	function lista_segmentos(){
-		$q = $this->db->select('vs.COD_DEPE, count(sa.RBD) total_colegios')
+		$q = $this->db->select('vs.SEGMENTO, count(sa.RBD) total_colegios')
 		->from('vista_segmentos vs')
-		->join('SEP_ACTUAL sa', 'vs.COD_DEPE = sa.COD_DEPE', 'LEFT')
-		->group_by('vs.COD_DEPE')
+		->join('SEP_ACTUAL sa', 'vs.SEGMENTO = sa.SEGMENTO', 'LEFT')
+		->group_by('vs.SEGMENTO')
 		->get();
 		return $q->result_array();
 	}
